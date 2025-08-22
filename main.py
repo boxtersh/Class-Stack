40cc81bd76317eadd6e425837feaba2ae2f1b439
Yuriy Terlyanskiy <boxtersh@yandex.ru>
2025-08-22 15:23:10 +0300

    fix

--- main.py
+++ main.py
@@ -5,16 +5,16 @@ class Stack:
             self.prev = prev
 
     def __init__(self):
-        self.__size = 0
+        self.__count = 0
         self.__top = None
 
-    def __check_is_empty(self) -> None:
+    def is_empty(self) -> None:
         """
         Функция проверяет стек на пустоту для функций pop и peek
         при пустом стеке безопасно завершает запрос возвращая None
         :return:
         """
-        if self.__size == 0: return
+        if self.__count == 0: return
 
     def push(self, elem) -> None:
         """
@@ -24,39 +24,32 @@ class Stack:
         """
         node = Stack.Node(elem)
 
-        if self.__top is None:
-            self.__top = node
+        if not self.is_empty():
+            node.prev = self.__top
 
-        else:
-            node.prev, self.__top = self.__top, node
-
-        self.__size += 1
+        self.__top =node
+        self.__count += 1
 
     def peek(self) -> None: #(хотел написать Self)
         """
         Функция получает значение поля data объекта с вершины стека
         :return: поле data объекта Node
         """
-        self.__check_is_empty()
-
-        return self.__top.data
+        if not self.is_empty():
+            return self.__top.data
 
     def pop(self) -> None: #(хотел написать Self)
         """
         Функция удаляет объект с вершины стека возвращая его поле data
         :return:
         """
-        self.__check_is_empty()
-        target = self.__top.data
-        self.__top, self.__size = self.__top.prev, self.__size - 1
+        if not self.is_empty():
+            target = self.__top.data
+            self.__top= self.__top.prev
+            self.__count -= 1
+            return target
 
-        return target
+    def __get_count(self):
+        return self.__count
 
-    def __get_size(self):
-        return self.__size
-
-    def __get_data(self):
-        return self.__top.data
-
-    size = property(__get_size)
-    data = property(__get_data)
+    count = property(__get_count)
--- test/test_class_stack.py
+++ test/test_class_stack.py
@@ -10,7 +10,7 @@ def test_stack_is_empty(stack):
 
     expected = 0
 
-    res = stack.size
+    res = stack.count
 
     assert expected == res, f'Ожидали:{expected}, получили:{res}'
 
@@ -21,7 +21,7 @@ def test_add_elem_in_stack_1(stack):
 
     stack.push(5)
 
-    res = stack.size
+    res = stack.count
 
     assert expected == res, f'Ожидали:{expected}, получили:{res}'
 
@@ -32,7 +32,7 @@ def test_add_elem_in_stack_2(stack):
 
     stack.push(5), stack.push(1), stack.push(13)
 
-    res = stack.size
+    res = stack.count
 
     assert expected == res, f'Ожидали:{expected}, получили:{res}'
 
@@ -56,7 +56,7 @@ def test_pop_elem_from_stack_1(stack):
     stack.push(5), stack.push(1), stack.push(13)
 
     res_1 = stack.pop()
-    res_2 = stack.size
+    res_2 = stack.count
 
     assert expected_1 == res_1, f'Ожидали:{expected_1}, получили:{res_1}'
     assert expected_2 == res_2, f'Ожидали:{expected_2}, получили:{res_2}'
@@ -71,21 +71,7 @@ def test_pop_elem_from_stack_2(stack):
 
     stack.pop(), stack.pop()
     res_1 = stack.pop()
-    res_2 = stack.size
+    res_2 = stack.count
 
     assert expected_1 == res_1, f'Ожидали:{expected_1}, получили:{res_1}'
     assert expected_2 == res_2, f'Ожидали:{expected_2}, получили:{res_2}'
-
-
-def test_get_data(stack):
-
-    expected = 13
-
-
-    stack.push(13)
-
-
-    res = stack.data
-
-
-    assert expected == res, f'Ожидали:{expected}, получили:{res}'
